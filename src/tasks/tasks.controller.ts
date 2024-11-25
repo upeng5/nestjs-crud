79d0abdc10bec5b6fd3b1268759fadb5d5bc6cf2
upeng5 <mousabmi@outlook.com>
2024-11-26 00:01:17 +0300

    Complete CRUD Functions

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param() params: any): Task {
+    return this.tasksService.getTaskById(params.id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+}
